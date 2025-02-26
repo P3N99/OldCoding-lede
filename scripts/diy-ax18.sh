e8c501369f13c5c68cf9826856d0af7cf076ffbb
P3N99 <145282517+P3N99@users.noreply.github.com>
2025-02-27 00:41:46 +0800

    Update diy-ax18.sh

--- scripts/diy-ax18.sh
+++ scripts/diy-ax18.sh
@@ -12,26 +12,22 @@ svn_export() {
 }
 
 # 删除冲突软件和依赖
-#rm -rf feeds/packages/lang/golang 
-#rm -rf feeds/luci/applications/luci-app-pushbot feeds/luci/applications/luci-app-serverchan
-
+rm -rf feeds/packages/lang/golang 
+git clone https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
 # 下载插件
-git clone --depth 1 https://github.com/gngpp/luci-theme-design package/luci-theme-design
+git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth 1 https://github.com/fw876/helloworld
-git clone https://github.com/chenmozhijin/luci-app-adguardhome
-# git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
-# git clone https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
-
-# svn_export "main" "luci-app-passwall" "package/luci-app-passwall" "https://github.com/xiaorouji/openwrt-passwall"
 
 # 安装插件
 ./scripts/feeds update -i
 ./scripts/feeds install -a
+set -x
 
 # 个性化设置
 cd package
-sed -i "s/OpenWrt /P3N9 build $(TZ=UTC-8 date "+%Y.%m.%d") @ Lean /g" lean/default-settings/files/zzz-default-settings
-#sed -i 's/OpenWrt/E8820S/' package/base-files/files/bin/config_generate
-#sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
+sed -i "s/LEDE /P3N9 build $(TZ=UTC-8 date "+%Y.%m.%d") @ Lean /g" lean/default-settings/files/zzz-default-settings
+sed -i 's/LEDE/ZN-M2/' package/base-files/files/bin/config_generate
+sed -i "/firewall\.user/d" lean/default-settings/files/zzz-default-settings
 sed -i 's/192.168.1.1/192.168.3.1/g' base-files/files/bin/config_generate
