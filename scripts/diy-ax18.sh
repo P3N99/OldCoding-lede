2b8aa0ee111b05ab93f4c7a7b044717a2549964b
P3N99 <145282517+P3N99@users.noreply.github.com>
2025-02-27 01:12:54 +0800

    Update diy-ax18.sh

--- scripts/diy-ax18.sh
+++ scripts/diy-ax18.sh
@@ -19,6 +19,7 @@ git clone --depth 1 https://github.com/fw876/helloworld package/helloworld
 git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall-packages
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
 
 # 安装插件
 ./scripts/feeds update -i
